9f10ef9dc5ccfafd180bad4f67a0dc8e7f08bb36
armandoltx <armando@armandoltx.com>
2017-03-14 19:22:54 +1100

    adding jquery

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,68 @@
-$(document).ready( function () {
-  console.log('test');
+$(document).ready(function() {
+  var result = 0;
+  var prevEntry = 0;
+  var operation = null;
+  var currentEntry = '0';
+  updateScreen(result);
+
+  $('.button').on('click', function(evt) {
+    var buttonPressed = $(this).html();
+//     console.log(buttonPressed);
+
+    if (buttonPressed === "C") {
+      result = 0;
+      currentEntry = '0';
+    } else if (buttonPressed === "CE") {
+      currentEntry = '0';
+
+    } else if (buttonPressed === "+/-") {
+      currentEntry *= -1;
+    } else if (buttonPressed === '.') {
+      currentEntry += '.';
+    } else if (isNumber(buttonPressed)) {
+      if (currentEntry === '0') currentEntry = buttonPressed;
+      else currentEntry = currentEntry + buttonPressed;
+    } else if (isOperator(buttonPressed)) {
+      console.log(buttonPressed);
+      prevEntry = parseFloat(currentEntry);
+      operation = buttonPressed;
+      currentEntry = '';
+    } else if(buttonPressed === '%') {
+      currentEntry = currentEntry / 100;
+    } else if (buttonPressed === 'sqrt') {
+      currentEntry = Math.sqrt(currentEntry);
+    } else if (buttonPressed === '1/x') {
+      currentEntry = 1 / currentEntry;
+    } else if (buttonPressed === 'pi') {
+      currentEntry = Math.PI;
+    } else if (buttonPressed === '=') {
+      currentEntry = operate(prevEntry, currentEntry, operation);
+      operation = '';
+    }
+
+    updateScreen(currentEntry);
+  });
 });
+
+var updateScreen = function(displayValue) {
+  var displayValue = displayValue.toString();
+  $('.screen').html(displayValue.substring(0, 10));
+};
+
+var isNumber = function(value) {
+  return !isNaN(value);
+}
+
+var isOperator = function(value) {
+  return value === '/' || value === '*' || value === '+' || value === '-';
+};
+
+var operate = function(a, b, operation) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  console.log(a, b, operation);
+  if (operation === '+') return a + b;
+  if (operation === '-') return a - b;
+  if (operation === '*') return a * b;
+  if (operation === '/') return a / b;
+}
